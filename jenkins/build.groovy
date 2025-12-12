ed34b3e8cff3d5193d7a39a1f4a53362d2a9c7a9
Ethan Clark <eclark@example.com>
2025-12-12 13:58:16 -0500

    Removed publish and modified build to upload to maven-releases

--- jenkins/build.groovy
+++ jenkins/build.groovy
@@ -16,9 +16,19 @@ pipeline {
         NEXUS_BASE          = "https://nexus.ethansclark.com"
         SNAPSHOT_REPO_ID    = "maven-snapshots"
         SNAPSHOT_REPO       = "${NEXUS_BASE}/repository/${SNAPSHOT_REPO_ID}/"
+        RELEASE_REPO_ID = "maven-releases"
+        RELEASE_REPO    = "${NEXUS_BASE}/repository/${RELEASE_REPO_ID}/"
 
         // --- DOCKER ---
         DOCKER_BASE     = "localhost:8003"
+    }
+
+    parameters {
+        choice(
+            name: 'BUMP VERSION',
+            choices: ['PATCH', 'MINOR', 'MAJOR'],\
+            description: 'Release bump A.B.C: PATCH->C, MAJOR_CHANGE->B, APP_CHANGE->A'
+        )
     }
 
     stages {
@@ -49,6 +59,48 @@ pipeline {
         stage('Containerize') {
             steps {
                 containerizeApp('maven', APP_NAME, SNAPSHOT_REPO, DOCKER_BASE, BUILD_TAG)
+            }
+        }
+
+        stage('Publish Release') {
+            when {
+                allOf {
+                    branch 'main'
+                    not { changeRequest() }
+                }
+            }
+
+            steps {
+                script {
+                    def groupId = sh(
+                        script: "mvn -q -DforceStdout help:evaluate -Dexpression=project.groupId",
+                        returnStdout: true
+                    ).trim()
+
+                    def artifactId = sh(
+                        script: "mvn -q -DforceStdout help:evaluate -Dexpression=project.artifactId",
+                        returnStdout: true
+                    ).trim()
+
+                    echo "[INFO] Maven coordinates: ${groupId}:${artifactId}"
+
+                    // - nexusLatestReleaseVersion(groupId, artifactId) -> returns latest or null
+                    // - bumpVersion(latest, bump) -> returns 1.0.0 if latest is null
+                    def latest = nexusLatestReleaseVersion(groupId, artifactId)
+                    def next   = bumpVersion(latest, params.BUMP)
+
+                    env.RELEASE_VERSION = next
+                    echo "[INFO] Latest release: ${latest ?: '(none)'} -> Next release: ${env.RELEASE_VERSION}"
+                }
+
+                sh """
+                    mvn -B -q versions:set \
+                        -DnewVersion=${RELEASE_VERSION} \
+                        -DgenerateBackupPoms=false
+                    """
+
+                // Publish to releases
+                containerizeApp('maven', APP_NAME, RELEASE_REPO, DOCKER_BASE, RELEASE_VERSION)
             }
         }
     }
