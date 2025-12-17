71da80f233a58a5acb0f1000b7ee7d48317672dc
Maykr1 <eclark72713@gmail.com>
2025-12-17 10:09:47 -0500

    Updating to new standard of releases

--- jenkins/publish.groovy
+++ jenkins/publish.groovy
@@ -1,5 +1,3 @@
-@Library('shared-jenkins-library') _
-
 pipeline {
     // --- SETUP ---
     agent any
@@ -13,8 +11,6 @@ pipeline {
         // --- MAVEN ---
         NEXUS               = credentials('nexus-deploy')
         NEXUS_BASE          = "https://nexus.ethansclark.com"
-        SNAPSHOT_REPO_ID    = "maven-snapshots"
-        SNAPSHOT_REPO       = "${NEXUS_BASE}/repository/${SNAPSHOT_REPO_ID}/"
         RELEASE_REPO_ID     = "maven-releases"
         RELEASE_REPO        = "${NEXUS_BASE}/repository/${RELEASE_REPO_ID}/"
 
@@ -47,14 +43,14 @@ pipeline {
             }
         }
 
-        stage('Publish Snapshot') {
+        stage('Publish Release') {
             steps {
                 script {
-                    env.SNAPSHOT_VERSION = getSnapshotVersion()
+                    env.RELEASE_VERSION = getReleaseVersion('maven')
                 }
 
-                setVersion('maven', env.SNAPSHOT_VERSION)
-                containerizeApp('maven', APP_NAME, SNAPSHOT_REPO, DOCKER_BASE, env.COMMIT_ID) // env.COMMIT_ID is set inside of getSnapshotVersion()
+                setVersion('maven', env.RELEASE_VERSION)
+                containerizeApp('maven', APP_NAME, RELEASE_REPO, DOCKER_BASE, env.RELEASE_VERSION)
             }
         }
     }
